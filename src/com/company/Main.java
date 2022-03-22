b3e3fd53a3ce1e5272ef39bc8cc57651db71e059
RodrigoPinheiro1 <rodrigopsps2015@gmail.com>
2022-03-22 16:19:36 -0300

    DesafioGftExer2

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Pessoa p1 = new Pessoa("Rodrigo", "rua joão pekny", "119999-99");
+        Pessoa p2 = new Pessoa("lucas", "Jabaquara", "11988-888");
+
+
+        System.out.println(p1.toString());
+        
+        System.out.println(p2.toString());
+
+
+    }
+}
